480e372456eb5f09af2c4996ee0ca9162d066aab
99003774 <78853952+99003774@users.noreply.github.com>
2021-02-15 09:39:00 +0530

    Update calculator_operations.c

--- 3_Implementation/calculator_operations.c
+++ 3_Implementation/calculator_operations.c
@@ -18,7 +18,34 @@ int areaOfRectangle() {
     float area = length*breadth;
     printf("The area of Rectangle is : %f", area);
 }
-
+void volumeOfCone() {
+    printf("CONE\n");
+    float radius, height;
+    printf("Enter the radius \n");
+    scanf("%f", &radius);
+    printf("Enter the height \n");
+    scanf("%f", &height);
+    float volume = (PI*radius*radius*height)/3;
+    printf("Volume of cone is : %f", volume);
+}
+void volumeOfSphere() {
+    printf("SPHERE\n");
+    float radius;
+    printf("Enter the radius \n");
+    scanf("%f", &radius);
+    float volume = (PI*radius*radius*radius)*4/3;
+    printf("Volume of Sphere is : %f", volume);
+}
+void volumeOfCylinder() {
+    printf("CYLINDER\n");
+    float radius,height;
+    printf("Enter the radius \n");
+    scanf("%f", &radius);
+    printf("Enter the height \n");
+    scanf("%f", &height);    
+    float volume = PI*radius*2*height;
+    printf("The volume of cylinder is : %f", volume);
+}
 
 int add(int operand1, int operand2)
 {
