a1804da7d0da588fc027bb0bc9cc5eafcbfedac8
99003774 <78853952+99003774@users.noreply.github.com>
2021-03-12 12:44:01 +0530

    updated header files by Kumar.

---
+++ 3_Implementation/inc/area_ku.h
@@ -0,0 +1,9 @@
+#ifndef __AREA_KU_H__
+#define __AREA_KU_H__
+
+#include <stdio.h>
+
+int areaOfSquare(float side);
+int areaOfRectangle(float length, float breadth);
+
+#endif
---
+++ 3_Implementation/inc/volume_ku.h
@@ -0,0 +1,10 @@
+#ifndef __VOLUME_KU_H__
+#define __VOLUME_KU_H__
+
+#include <stdio.h>
+
+int volumeOfCone(float radius, float height);
+int volumeOfSphere(float radius);
+int volumeOfCylinder(float radius,float height);
+
+#endif
