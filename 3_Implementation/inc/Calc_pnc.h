534901618dbbc17bcc7273999c8f89de16eac562
99003772 <78854464+99003772@users.noreply.github.com>
2021-03-13 09:24:14 +0530

    Updated header file.

---
+++ 3_Implementation/inc/Calc_pnc.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+#ifndef __CALC_PNC_H__
+#define __CALC_PNC_H__
+
+long permutation(int n, int r);
+long combination(int n, int r);
+long factorial(int x);
+
+
+#endif  /* #define __CALC_PNC_H__ */
