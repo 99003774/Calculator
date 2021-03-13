d4c4d6c21339f2cac60293d4d98c4de8af46a4a7
99003772 <78854464+99003772@users.noreply.github.com>
2021-03-13 09:25:10 +0530

    Updated header file for arithmetic operations.

---
+++ 3_Implementation/inc/Calc_ari.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+#ifndef __CALC_ARI_H__
+#define __CALC_ARI_H__
+
+int add(float s1,float s2);
+int sub(float s3,float s4);
+int mul(float s7,float s8);
+float divs(float s5,float s6);
+
+#endif  /* #define __CALC_ARI_H__ */
