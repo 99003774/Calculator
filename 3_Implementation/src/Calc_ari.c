d89d73baa9b3772512be547f6b0c2f6752f4ec5e
99003772 <78854464+99003772@users.noreply.github.com>
2021-03-13 09:27:51 +0530

    Updated code for arithmetic.

---
+++ 3_Implementation/src/Calc_ari.c
@@ -0,0 +1,41 @@
+#include "Calc_ari.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int add(float s1,float s2)
+{
+
+    float a;
+    a=s1+s2;
+    return a;
+
+}    
+int sub(float s3,float s4)
+{
+
+    float b;
+    b=s3-s4;
+    return b;
+
+}    
+
+
+int mul(float s7, float s8)
+{
+   
+    float a; 
+    a= s7*s8;
+    return a;   
+
+}
+
+float divs(float s5, float s6)
+{
+
+    float sp;
+    sp= s5/s6;
+    return sp;  
+
+}
